a1adc9f45a78330b54fba9fd77a4b5db8f622ab0
Sonicaii <41628956+Sonicaii@users.noreply.github.com>
2025-03-25 16:11:15 +1100

    Implement File

--- app/src/main/java/com/unimelb/swen30006/workshops/File.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/File.java
@@ -1,13 +1,25 @@
 package com.unimelb.swen30006.workshops;
 
+import java.util.Date;
+
 // Sample private file class to be replaced by your implementaiton
 class File {
-    public String fileType(){
-        double num = Math.random();
-        if(num<0.5){
-            return "pdf";
-        } else {
-            return "docx";
-        }
+    private Date createdDate;
+    private String fileData;
+    private String fileName;
+
+    // Missing
+    private String fileType;
+
+    public String fileType() {
+        return fileType;
+    }
+
+    public int fileSize() {
+        return fileData.length();
+    }
+
+    public String content() {
+         return fileData;
     }
 }
